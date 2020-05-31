a85be5cafe11fa1981a7ed2aefdba0615f00b23f
luisrs8608 <35710163+luisrs8608@users.noreply.github.com>
2020-05-31 03:10:15 -0300

    Creating eCommerce: charge payment fee

---
+++ website_sale_charge_payment_fee/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+{
+    "name": "eCommerce: charge payment fee",
+    "summary": "Payment fee charged to customer",
+    "version": "13.0.1",
+    "author": "AITIC",
+    "website": "http://www.aitic.com.ar",
+    "license": "AGPL-3",
+    "category": "Website",
+    "depends": ["website_sale"],
+    "data": [
+        "views/payment_view.xml",
+        "templates/website_sale.xml"
+    ],
+    "application": False,
+    "installable": True,
+}
